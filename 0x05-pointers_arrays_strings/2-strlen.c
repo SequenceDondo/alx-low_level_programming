162565447f70101203350c410e155e800da9f5f6
SequenceDondo <sequelacompany@gmail.com>
2023-02-27 01:08:40 -0800

    Write a function that returns the length of a string.
    Prototype: int _strlen(char *s);
    FYI: The standard library provides a similar function: strlen. Run man strlen to learn more.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+
+/**
+ * * _strlen - main funtion that take a string as an argument
+ * * @s: Inputed argument
+ * * Return: Returns the lenghth of an inputed robber
+ * */
+int _strlen(char *s)
+{
+		int i;
+
+			for (i = 0; s[i] != '\0'; i++)
+					{
+								i++;
+									}
+				return (i);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
