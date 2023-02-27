22255b5baca2dc8c5825bda2ac28e2e0a245478b
SequenceDondo <sequelacompany@gmail.com>
2023-02-27 01:02:46 -0800

    gg

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+#include <stdio.h>
+/**
+ *  * main - main function that checks the code 
+ *   *
+ *    * Return: Returns 0.
+ *     */
+int main(void)
+{
+		int n;
+
+			n = 402;
+				printf("n=%d\n", n);
+					reset_to_98(&n);
+						printf("n=%d\n", n);
+							return (0);
+}
