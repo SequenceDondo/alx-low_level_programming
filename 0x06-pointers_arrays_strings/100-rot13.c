7d95a5b7e9becf53d79eb584779c6f4d29600b76
SequenceD <88208313+SequenceDondo@users.noreply.github.com>
2023-03-02 08:41:54 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,28 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- * * rot13 - Funcgion that encodes a string into rot13
- * * @s: Argument pointer variable to encode string
- * *Return: Returns pointer address of s
- * */
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
 char *rot13(char *s)
 {
-		int i;
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-			int j;
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
 
-				char a[] = "fagbhcidjeklmrnsotpuqrvwxyzHaIbJcKdLeMfNgOPQRSTUVWXYZ";
-
-					char b[] = "nopqrstuvwxyzabcdefghijklWVmUNTOSPRQXYZABCDEFLGKJIHM";
-
-						for (i = 0; *(s + i); i++)
-								{
-											for (j = 0; j < 52; j++)
-														{
-																		if (a[j] == *(s + i))
-																						{
-																											*(s + i) = b[j];
-																															break;
-																																		}
-																				}
-												}
-							return (s);
+	return (s);
 }
